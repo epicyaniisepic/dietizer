c6f3404855713e62084113e7f4c14323e5f327e9
epicyaniisepic <yanielat@gmail.com>
2016-11-29 17:46:40 +0800

    Added routes and creates views per tab

--- src/js/main-routes.js
+++ src/js/main-routes.js
@@ -12,20 +12,42 @@
 		$stateProvider
 			.state('/', {
 				url:'/',
-				template: '<div layout-padding> <h1> Landing Page </h1> </div>'
+				// template: '<div layout-padding> <h1> Landing Page </h1> </div>'
+				template: '<div layout-padding> <h1> Landing / Home </h1> </div>',
+			})
+			.state('home', {
+				url: '/home',
+				// templateUrl: 'assets/views/todo.html',
+				template: '<div layout-padding> <h1> Home </h1> </div>',
+				// controller: 'TodoCtrl'
+				controller: function($scope) {}
+			})
+			.state('about', {
+				url: '/about',
+				templateUrl: 'src/html/about.html',
+				// template: '<div layout-padding> <h1> About </h1> </div>',
+				// controller: 'TodoCtrl'
+				controller: function($scope) {}
 			})
 			.state('ultimate', {
 				url: '/ultimate',
-				// templateUrl: 'assets/views/home.html',
-				template: '<div layout-padding> <h1> Ultimate Optimizer </h1> </div>',
+				templateUrl: 'src/html/ultimate.html',
+				// template: '<div layout-padding> <h1> Ultimate Optimizer </h1> </div>',
 				controller: function($scope) {}
 			})
 			.state('diet', {
 				url: '/diet',
-				// templateUrl: 'assets/views/todo.html',
-				template: '<div layout-padding> <h1> Dietizer </h1> </div>',
+				templateUrl: 'src/html/diet.html',
+				// template: '<div layout-padding> <h1> Dietizer </h1> </div>',
 				// controller: 'TodoCtrl'
 				controller: function($scope) {}
-			});
+			})
+			.state('references', {
+				url: '/references',
+				// templateUrl: 'assets/views/todo.html',
+				template: '<div layout-padding> <h1> References </h1> </div>',
+				// controller: 'TodoCtrl'
+				controller: function($scope) {}
+			})
 		});
 })();
